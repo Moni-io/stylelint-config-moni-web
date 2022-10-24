64f704715ba3fe99991d5b3558ae73bda543c6cd
Qmzik <nikita.lebedev20812@gmail.com>
2022-10-24 17:27:05 +0400

    update rules

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ module.exports = {
     rules: {
         "indentation": 4,
         "property-no-vendor-prefix": null,
-        "color-function-notation": "legacy",
+        "color-function-notation": "off",
+        "custom-property-pattern": "^([a-z]{1,})(([A-Z]{1}[a-z]*)|[0-9]{1,})*$",
         "alpha-value-notation": "number",
         "selector-class-pattern": "^[a-z][a-zA-Z0-9]+$",
         "color-hex-case": "upper",
+        "declaration-block-no-redundant-longhand-properties": [{
+            "ignoreShorthands": ["grid-gap", "font"]
+        }],
+        "scss/at-extend-no-missing-placeholder": "off",
+        "string-quotes": "single",
+        "max-line-length": 150,
         "at-rule-no-unknown": [true, {
             "ignoreAtRules": ["function", "if", "each", "include", "mixin", "extend"]
         }]
